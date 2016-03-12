f6d7ccb588a4fc0ccb9b0e8090c10ab8ed56e315
Martin Lehmann <lehmar11@student.westerdals.no>
2016-03-12 09:59:41 +0100

    Use real pictures.

--- User.js
+++ User.js
@@ -3,19 +3,19 @@ const util = require('./util.js');
 const users = [{
   username: 'andreas',
   name: 'Andreas',
-  profilePicture: 'http://www.cbc.ca/smartestperson/content/image/avatar-placeholder.png',
+  profilePicture: 'https://scontent-arn2-1.xx.fbcdn.net/hphotos-xfl1/v/t1.0-9/10479542_10152575819385491_7575197136310777683_n.jpg?oh=1cfaaee6833efd2235acb74b42bccbf5&oe=5757DC22',
   friends: ['håkon', 'martin'],
   destinations: ['Spain', 'Oslo', 'Bergen']
 }, {
   username: 'martin',
   name: 'Martin',
-  profilePicture: 'http://askfedoratest-anuradhaw.rhcloud.com/static/askfedoratheme/images/man.png',
+  profilePicture: 'https://scontent-arn2-1.xx.fbcdn.net/hphotos-xaf1/v/t1.0-9/10351759_10152203958431517_379246105981390468_n.jpg?oh=65657f9ba8415a8f2eea6727fbe85a35&oe=574B4317',
   friends: ['håkon', 'andreas'],
   destinations: ['Spain', 'LA', 'Oslo']
 }, {
   username: 'håkon',
   name: 'Håkon',
-  profilePicture: 'http://www.cbc.ca/smartestperson/content/image/avatar-placeholder.png',
+  profilePicture: 'https://scontent-arn2-1.xx.fbcdn.net/hphotos-xtp1/t31.0-8/12401909_10153467542424285_1408498897537394327_o.jpg',
   friends: ['andreas', 'martin'],
   destinations: ['Oslo', 'Bergen']
 }];
