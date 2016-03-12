39ede8b134abfde207488d292808a9719f012e8e
Martin Lehmann <lehmar11@student.westerdals.no>
2016-03-12 03:59:01 +0100

    Serve an array of images.

--- Destination.js
+++ Destination.js
@@ -1,12 +1,39 @@
 const util = require('./util.js');
 
-const destinations = [
-  {name: 'Madrid', image: 'http://madride.net/wp-content/uploads/2015/04/Madrid-City-at-Night.jpg'},
-  {name: 'LA', image: 'http://archpaper.com/uploads/image/los_angeles_city_01.jpg'},
-  {name: 'Gothenburg', image: 'http://www.accommodationengine.co.uk/files/cityguide/full/gothenburg1.jpg'},
-  {name: 'Oslo', image: 'http://www.casayego.com/europeancities/oslo/oslo-c.jpg'},
-  {name: 'Bergen', image: 'https://res-1.cloudinary.com/simpleview/image/upload/c_fill,f_auto,q_65,w_768/v1/clients/norway/zachariasbryggen-bergen-norway_315f239b-9f2a-4bd2-b9db-e9fe510f5449.jpg'},
-];
+const destinations = [{
+  name: 'Madrid',
+  images: [{
+    title: 'Madrid City at Night',
+    url: 'http://madride.net/wp-content/uploads/2015/04/Madrid-City-at-Night.jpg',
+  }, {
+    title: 'Gothenburg 1',
+    url: 'http://www.accommodationengine.co.uk/files/cityguide/full/gothenburg1.jpg',
+  }]
+}, {
+  name: 'LA',
+  images: [{
+    title: 'Los Angeles City 01',
+    url: 'http://archpaper.com/uploads/image/los_angeles_city_01.jpg',
+  }],
+}, {
+  name: 'Gothenburg',
+  images: [{
+    title: 'Gothenburg 1',
+    url: 'http://www.accommodationengine.co.uk/files/cityguide/full/gothenburg1.jpg',
+  }],
+}, {
+  name: 'Oslo',
+  images: [{
+    title: 'Oslo C',
+    url: 'http://www.casayego.com/europeancities/oslo/oslo-c.jpg',
+  }],
+}, {
+  name: 'Bergen',
+  images:[{
+    title: 'Zachariasbryggen',
+    url: 'https://res-1.cloudinary.com/simpleview/image/upload/c_fill,f_auto,q_65,w_768/v1/clients/norway/zachariasbryggen-bergen-norway_315f239b-9f2a-4bd2-b9db-e9fe510f5449.jpg'
+  }],
+}];
 
 module.exports = {
   all: () => destinations,
